4314ab00aebf0b60003331e311073081db927b98
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-19 14:26:36 +0200

    Fetch and parse GQ video games webpage

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get(url="https://www.gq-magazine.co.uk/article/best-video-games-all-time")
+web_page = response.text
+
+soup = BeautifulSoup(web_page, 'html.parser')
